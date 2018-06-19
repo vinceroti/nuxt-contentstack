cdf95068abff026e2ccc02cb7eb86c6cecbc2b68
Vince Roti <vince.roti@gmail.com>
2018-06-19 15:21:12 -0700

    second page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <router-link :to="{ name: 'testpage' }">test</router-link>
     <div v-html="bio"/>
-    <img :src="img"/>
+    <img src="http://i1.adis.ws/i/k2skis/tubbs_1718_flex-trk-mens_pivot.jpg?h=300&w=400"/>
     <div v-html="imgdata"/>
   </div>
 </template>
@@ -11,23 +12,16 @@ import Stack from '~/plugins/entry'
 import axios from 'axios'
 
 export default {
-  data () {
-    return {
-      bio: '',
-      img: 'http://i1.adis.ws/i/k2skis/tubbs_1718_flex-trk-mens_pivot.jpg?h=300&w=400',
-      imgdata: ''
-    }
-  },
   async asyncData() {
     let res = await Stack.getEntry('team_members', 'blt3ec29586cf35b4d6')
-    let imgdata;
+    let imgdata
     try {
-      imgdata = await axios.get('http://i1.adis.ws/i/k2skis/tubbs_1718_flex-trk-mens_pivot.json');
-      console.log(imgdata.data);
+      imgdata = await axios.get('http://i1.adis.ws/i/k2skis/tubbs_1718_flex-trk-mens_pivot.json')
+      imgdata = imgdata.data
     } catch (error) {
       console.error(error);
     }
-    return { entry: res, bio: res.bio, imgdata: imgdata.data }
+    return { entry: res, bio: res.bio, imgdata: imgdata }
   }
 }
 </script>
---
+++ pages/testpage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-link :to="{ name: 'index' }">Index</router-link>
+    <h1 v-html="title"/>
+    <div v-html="body"/>
+    <img :src="img"/>
+  </div>
+</template>
+
+<script>
+import Stack from '~/plugins/entry'
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    let res = await Stack.getEntry('technology', 'blt74c9a141281d9f99')
+
+    return { entry: res, body: res.technology_description, title: res.technology_title, img: res.technology_image }
+  }
+}
+</script>
+
+<style>
+
+</style>
