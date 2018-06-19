3ca873926446674eedda4968697ce8513057d4ea
Vince Roti <vince.roti@gmail.com>
2018-06-19 11:48:51 -0700

    axios image json

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
-    <p v-html="bio"/>
+    <div v-html="bio"/>
     <img :src="img"/>
+    <div v-html="imgdata"/>
   </div>
 </template>
 
 <script>
 import Stack from '~/plugins/entry'
+import axios from 'axios'
 
 export default {
   data () {
     return {
       bio: '',
-      img: 'http://i1.adis.ws/i/k2skis/tubbs_1718_flex-trk-mens_pivot.jpg?h=300&w=400'
+      img: 'http://i1.adis.ws/i/k2skis/tubbs_1718_flex-trk-mens_pivot.jpg?h=300&w=400',
+      imgdata: ''
     }
   },
   async asyncData() {
     let res = await Stack.getEntry('team_members', 'blt3ec29586cf35b4d6')
-    return { entry: res, bio: res.bio }
+    let imgdata;
+    try {
+      imgdata = await axios.get('http://i1.adis.ws/i/k2skis/tubbs_1718_flex-trk-mens_pivot.json');
+      console.log(imgdata.data);
+    } catch (error) {
+      console.error(error);
+    }
+    return { entry: res, bio: res.bio, imgdata: imgdata.data }
   }
 }
 </script>
